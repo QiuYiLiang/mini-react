b10e4db42ff6070040627aaadcb6ab2df17d9dc8
qiuyiliang <pengwenlong245@gmail.com>
2024-03-20 21:41:17 +0800

    1

--- examples/app/src/main.tsx
+++ examples/app/src/main.tsx
@@ -8,5 +8,12 @@ const b = (
     <p>2</p>
   </button>
 );
+const c = (
+  <button>
+    <p>1</p>
+    <p>3</p>
+  </button>
+);
 
 MiniReact.createRoot(document.getElementById("root") as HTMLElement).render(b);
+MiniReact.createRoot(document.getElementById("root") as HTMLElement).render(c);
