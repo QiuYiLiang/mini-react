c443c4eec3f2d393cb22ba1f304ba04ea60c88bf
qiuyiliang <pengwenlong245@gmail.com>
2024-03-20 23:39:13 +0800

    1

--- examples/app/src/main.tsx
+++ examples/app/src/main.tsx
@@ -3,12 +3,18 @@ import MiniReact from "@qiuyl/mini-react";
 
 const C = () => {
   const [count, setCount] = MiniReact.useState(0);
+  const [display, setDisplay] = MiniReact.useState(false);
+  const toggle = () => {
+    setDisplay((display) => !display);
+  };
   return (
     <button
       onClick={() => {
         setCount((count) => count + 1);
+        toggle();
       }}
     >
+      {display && <h1>hhh</h1>}
       {count}
     </button>
   );
