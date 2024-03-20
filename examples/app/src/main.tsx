31364b75d7ea5bc94652f46c2508e52a3e377bdd
pengwl <pengwenlong245@gmail.com>
2024-03-20 11:06:42 +0800

    1

--- examples/app/src/main.tsx
+++ examples/app/src/main.tsx
@@ -2,5 +2,6 @@
 import MiniReact from "@qiuyl/mini-react";
 
 const a = <button>click</button>
+const b = <button><p>1</p><p>2</p></button>
 
-console.log(a, MiniReact)
+console.log(a, b, MiniReact)
--- packages/mini-react/src/index.ts
+++ packages/mini-react/src/index.ts
@@ -1,8 +1,33 @@
-function createElement() {
-    console.log(arguments)
-    return 123
+interface MiniReactElement {
+  type: string;
+  props: Record<string, any>;
+}
+
+type Children = (MiniReactElement | string)[];
+
+function createElement(
+  type: string,
+  props: Record<string, any>,
+  ...children: Children
+) {
+  return {
+    type,
+    props: {
+      ...props,
+      children: children.map((element) => {
+        typeof element === "string"
+          ? {
+              type: "TEXT_ELEMENT",
+              props: {
+                nodeValue: element,
+              },
+            }
+          : element;
+      }),
+    },
+  };
 }
 
 export default {
-    createElement
-}
+  createElement,
+};
