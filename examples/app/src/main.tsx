f5cb2880fcc914a876f458fc936e3be59395967c
pengwl <pengwenlong245@gmail.com>
2024-03-21 11:49:07 +0800

    1

--- examples/app/src/main.tsx
+++ examples/app/src/main.tsx
@@ -7,16 +7,25 @@ const C = () => {
   const toggle = () => {
     setDisplay((display) => !display);
   };
+  const [todoList, setTodoList] = MiniReact.useState([1, 2, 3, 4]);
   return (
-    <button
-      onClick={() => {
-        setCount((count) => count + 1);
-        toggle();
-      }}
-    >
-      {display && <h1>hhh</h1>}
-      {count}
-    </button>
+    <div>
+      <button
+        onClick={() => {
+          setCount((count) => count + 1);
+          toggle();
+          setTodoList([31, 13, 14, 51, 6, 567, 4, 7, 1124, 2]);
+        }}
+      >
+        {display && <h1>hhh</h1>}
+        {count}
+      </button>
+      <ul>
+        {todoList.map((v) => {
+          return <li>{v}</li>;
+        })}
+      </ul>
+    </div>
   );
 };
 
